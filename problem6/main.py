e768e0ca2e8dc59fa09cd24beb55c6c9edb3cdbe
ihsanr <ihsan.rais22@gmail>
2024-05-06 20:44:40 +0700

    Fix Update Tugas Part 3

--- problem6/main.py
+++ problem6/main.py
@@ -1,15 +1,28 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 def full_prima(N):
-    # your code here
-    return ''
+    if N < 0:
+        return False
+    N_str = str(N)
+    for digit in N_str:
+        if not is_prime(int(digit)):
+            return False
+    return True
 
 if __name__ == '__main__':
-    print(full_prima(2)) # True
-    print(full_prima(3)) # True
-    print(full_prima(11)) # False
-    print(full_prima(13)) # False
-    print(full_prima(23)) # True
-    print(full_prima(29)) # False
-    print(full_prima(37)) # True
-    print(full_prima(41)) # False
-    print(full_prima(43)) # False
-    print(full_prima(53)) # True
+    print(full_prima(2))  # True
+    print(full_prima(3))  # True
+    print(full_prima(11))  # False
+    print(full_prima(13))  # False
+    print(full_prima(23))  # True
+    print(full_prima(29))  # False
+    print(full_prima(37))  # True
+    print(full_prima(41))  # False
+    print(full_prima(43))  # False
+    print(full_prima(53))  # True
